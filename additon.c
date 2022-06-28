331d3e070563b197ea25cd324dab7ab5f97dc428
Shivam Mahajan <pro.dev.shivay@gmail.com>
2022-06-28 20:42:40 +0530

    Added somne new files

---
+++ additon.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter two Numbers\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The addition of given number is %d ",c);
+    
+    return 0;
+}
