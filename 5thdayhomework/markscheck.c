8a0de48206027d377092eba7f22493b19f0dff92
Shivam Mahajan <pro.dev.shivay@gmail.com>
2022-07-06 13:08:13 +0530

    5thdayhomework

---
+++ 5thdayhomework/markscheck.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter Your Number");
+    scanf("%d",&x);
+    if(x>=60){
+       printf("Your pass in 1st division");
+    }
+          else
+            {
+              if(x>=45)
+                printf("Your are passs in 2nd divisioin");
+              else
+               if(x<=33)
+                   printf("your are fail");
+            }
+
+
+    return 0;
+}
