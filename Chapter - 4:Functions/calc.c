1f1b4e16a16ad22f7445d9ec32079ce17edfb5d7
Shivam Mahajan <pro.dev.shivay@gmail.com>
2022-07-16 13:24:21 +0530

    simplecalc

---
+++ Chapter - 4:Functions/calc.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int add(int a , int b)
+{
+  return(a+b);
+}
+int sub(int a , int b)
+{
+ return (a-b);
+}
+int multi(int a , int b)
+{
+  return (a*b);
+}
+int divi(int a , int b)
+{
+  return (a/b);
+}
+
+int main()
+{
+   int a,b,choice;
+  do
+ {
+   printf("\n1.Addition \n2.Substraction \n3.Multiplication \n4.Division \n5.Exit\n\n");
+   printf("\nEnter your Choice\n");
+   scanf("%d",&choice);
+   if(choice<=5)
+  {
+    if(choice == 5)
+    {
+      printf("Select right Choice");
+      break;
+    }
+    printf("Enter two Number\n");
+    scanf("%d%d",&a,&b);
+     switch(choice)
+      {
+       case 1:
+        printf("%d",add(a,b));
+        break;
+       case 2:
+        printf("%d",sub(a,b));
+        break;
+       case 3:
+        printf("%d",multi(a,b));
+        break;
+       case 4:
+        printf("%d",divi(a,b));
+        break;
+     }
+  }
+  else
+  {
+    printf("Please select right choice");
+  }
+ } while(1);
+
+}
