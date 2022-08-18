55b71faa138ae0165bfc40b98a07177f4978b616
Shivam Mahajan <pro.dev.shivay@gmail.com>
2022-08-18 13:22:58 +0530

    loopsinc

--- Chapter - 2: Loops/inverted triangle.c
+++ Chapter - 2: Loops/inverted triangle.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
     int i,j;
-    for(i=1;i<=6;i++){
+    for(i=1;i<=6;i++)
+    {
 
-        for(j=1;j<=6;j++){
+        for(j=1;j<=6;j++)
+        {
              if(j>=1 && j<=7-i)
                 printf(" * ");
              else
