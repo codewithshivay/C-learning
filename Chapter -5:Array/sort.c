45b9c69ca8bd93185911389b2dcceb72390ee562
Shivam Mahajan <pro.dev.shivay@gmail.com>
2022-08-29 15:21:13 +0530

    Arraysinc

--- Chapter -5:Array/sort.c
+++ Chapter -5:Array/sort.c
@@ -1,12 +1,26 @@
-//Write a program to sort elements of an array...!!
+//program to sort array elements in ascending or descending order..!!
 #include <stdio.h>
+void sort(int a[],int N)
+{
+    int i,r,t;
+    for(r=0; r<=N-1; r++)
+    {
+        for(i=0; i<=N-1-r; i++)
+         if(a[i] > a[i+1])
+         {
+             t = a[i];
+             a[i] = a[i+1];
+             a[i+1] = t;
+         }
+    }
+}
 int main()
 {
-  int a[10],i;
-  printf("Enter 10 array element :\n");
-  for(i=0; i<10; i++)
-    scanf("%d",&a[i]);
-
+    int a[]={2,5,9,34},i;
+      sort(a , 3);
+    for(i=0; i<4; i++)
+       printf(" %d",a[i]);
 
   return 0;
 }
++
