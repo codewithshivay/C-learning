04d948728e870622fd7387ab364c5111b99c0c1d
Shivam Mahajan <pro.dev.shivay@gmail.com>
2022-09-16 11:24:19 +0530

    2homework

--- 2nddayhomework/maximum.c
+++ 2nddayhomework/maximum.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c,max;
-    printf("Enter three Number");
+    int a,b,c,maxi;
+    printf("Enter three Number : ");
     scanf("%d%d%d",&a,&b,&c);
-    max = (a>b)?((a>c)?a:c) : ((b>c)?b:c);
-    printf("Maximum number is %d",max);
+    maxi = (a>b)?((a>c)?a:c) : ((b>c)?b:c);
+    printf("Maximum number is %d",maxi);
 
     return 0;
 }
